104d17d17840e3a2c54a97662f0a240943f625e3
mwrazam <37275578+mwrazam@users.noreply.github.com>
2021-03-05 08:31:58 -0800

    Point projection function

--- run.py
+++ run.py
@@ -10,7 +10,31 @@ def get_parallel_line(l1):
     pass
 
 def point_projection(segment, p):
-    # get points for line segment
+
+    # transform segment and point to be vectors from origin
+    x0, y0 = segment[0]
+    x1, y1 = segment[1][0] - x0, segment[1][1] - y0
+    px, py = p[0] - x0, p[1] - y0
+    projected_point = [x0, y0] # start the projection at the origin
+
+    #print(f"orgin: ({x0},{y0}), segment: ({x1},{y1}), point: ({px},{py})")
+
+    # S: Line segment vector to project onto, P: Vector from origin to point p
+    S, P = np.array([x1,y1]), np.array([px, py])
+
+    # Scalar projection of P onto S given by P . (S / ||S||)
+    norm_S = S / np.sqrt(S.dot(S)) # the normal vector of S
+    proj = P.dot(norm_S) # the projection of P onto S
+
+    # Add back the origin translation to the projected point
+    projected_point = projected_point + (proj * norm_S)
+
+    return projected_point
+    
+
+
+    
+    '''# get points for line segment
     x1, x2 = segment[0][0], segment[1][0]
     y1, y2 = segment[0][1], segment[1][1]
     x_p, y_p = p[0], p[1]
@@ -25,7 +49,7 @@ def point_projection(segment, p):
     m_perp = -(1 / m)
     b_perp = y_p - (x_p * m_perp)
 
-    s = [y2 - y1, x2 - x1]
+    s = [y2 - y1, x2 - x1]'''
 
 
 
@@ -60,7 +84,13 @@ def run():
         plt.plot(l1_x, l1_y, 'bo-')
         plt.plot(l2_x, l2_y, 'ro-')
 
-    point_projection(l2[0:2],l1[0])
+    proj = point_projection(l2[0:2],l1[0])
+    proj2 = point_projection(l2[0:2],l1[1])
+    proj3 = point_projection(l2[0:2],l1[2])
+
+    plt.plot(proj[0], proj[1], 'go-')
+    plt.plot(proj2[0], proj2[1], 'go-')
+    plt.plot(proj3[0], proj3[1], 'go-')
 
     plt.show()
 
