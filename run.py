9dce749d781de3cd691440190380c9c386b37d8a
mwrazam <37275578+mwrazam@users.noreply.github.com>
2021-03-08 12:48:32 -0800

    Fixed projection points

--- run.py
+++ run.py
@@ -3,66 +3,34 @@ import os, json
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_pts(points, color, thickness):
-    pass
-
-def get_parallel_line(l1):
-    pass
-
-def point_projection(segment, p):
-
-    # transform segment and point to be vectors from origin
-    x0, y0 = segment[0]
-    x1, y1 = segment[1][0] - x0, segment[1][1] - y0
-    px, py = p[0] - x0, p[1] - y0
-    projected_point = [x0, y0] # start the projection at the origin
-
-    #print(f"orgin: ({x0},{y0}), segment: ({x1},{y1}), point: ({px},{py})")
-
-    # S: Line segment vector to project onto, P: Vector from origin to point p
-    S, P = np.array([x1,y1]), np.array([px, py])
-
-    # Scalar projection of P onto S given by P . (S / ||S||)
-    norm_S = S / np.sqrt(S.dot(S)) # the normal vector of S
-    proj = P.dot(norm_S) # the projection of P onto S
-
-    # Add back the origin translation to the projected point
-    projected_point = projected_point + (proj * norm_S)
-
-    return projected_point
-    
-
-
-    
-    '''# get points for line segment
-    x1, x2 = segment[0][0], segment[1][0]
-    y1, y2 = segment[0][1], segment[1][1]
-    x_p, y_p = p[0], p[1]
-
-    # get equation of line segment: y = mx + b
-    m = (y2-y1)/(x2-x1)
-    b = y1 - (x1 * m)
-
-    print(b)
-
-    # determine equation for perpendicular line
-    m_perp = -(1 / m)
-    b_perp = y_p - (x_p * m_perp)
-
-    s = [y2 - y1, x2 - x1]'''
-
-
-
-
-    
-
-def get_intersection_points(l1, l2):
-    pass
-
 def load_data(filename):
     with open(filename, 'r') as f:
         return json.load(f)
     return False
+
+# @segment: 2 point list of start and end points comprising segment
+# @p : The point we want to project onto the segment
+def point_projection(segment, p):
+    projection = None
+    has_projection = False # if the projection is outside of the segment, we can't project it
+    double_vertex = False # if we get this, we just proceed in the list and ignore this point
+
+    # Transform both into vectors, re-origin for vector calculations
+    S, P = np.array(np.subtract(segment[1],segment[0])), np.array(np.subtract(p,segment[0]))
+
+    # Determine projection
+    norm_S = S / np.sqrt(S.dot(S)) # the normal vector of S
+    proj = P.dot(norm_S) # the projection of P onto S
+
+    if proj <= 0:
+        double_vertex = True
+
+    if proj <= np.sqrt(S.dot(S)):
+        has_projection = True
+        projected_point = segment[0] + (proj * norm_S) # add back origin
+        projection = [p, projected_point]
+
+    return has_projection, projection, double_vertex
 
 def run():
     data_file = os.path.join(os.getcwd(), "sample-data", "sample-data.json")
@@ -77,6 +45,7 @@ def run():
     ax.set_xticks(np.arange(0, 30, 1))
     ax.set_yticks(np.arange(0, 17, 1))
 
+    #l1, l2 = np.array(data['lines'][0]), np.array(data['lines'][1])
     l1, l2 = data['lines'][0], data['lines'][1]
     l1_x, l1_y = [p[0] for p in l1], [p[1] for p in l1]
     l2_x, l2_y = [p[0] for p in l2], [p[1] for p in l2]
@@ -84,14 +53,47 @@ def run():
         plt.plot(l1_x, l1_y, 'bo-')
         plt.plot(l2_x, l2_y, 'ro-')
 
-    proj = point_projection(l2[0:2],l1[0])
-    proj2 = point_projection(l2[0:2],l1[1])
-    proj3 = point_projection(l2[0:2],l1[2])
+    current_p = 0
+    for i in range(len(l2) - 1): # for each segment in the reference line
+        print(f"Segment {i} on reference line: ({l2[i]},{l2[i+1]})")
+        s1, s2 = l2[i], l2[i+1]
+        
+        while current_p < len(l1):
+            p = l1[current_p]
+            has_projection, proj, dbv = point_projection([s1, s2], p)
 
-    plt.plot(proj[0], proj[1], 'go-')
-    plt.plot(proj2[0], proj2[1], 'go-')
-    plt.plot(proj3[0], proj3[1], 'go-')
+            print(f"At index {current_p} Point {p}: {has_projection}")
 
+            if has_projection and not dbv:
+                p_x1, p_x2 = proj[0][0], proj[1][0]
+                p_y1, p_y2 = proj[0][1], proj[1][1]
+                plt.plot([p_x1, p_x2], [p_y1, p_y2], 'go--')
+                current_p = current_p + 1
+            elif dbv: # don't plot a double vertex, handle it in the reverse pass
+                current_p = current_p + 1
+            else:
+                break
+    '''current_p = 0
+    for i in range(len(l1) - 1): # for each segment in the reference line
+        print(f"Segment {i} on reference line: ({l1[i]},{l1[i+1]})")
+        s1, s2 = l1[i], l1[i+1]
+        
+        while current_p < len(l2):
+            p = l2[current_p]
+            has_projection, proj, dbv = point_projection([s1, s2], p)
+
+            print(f"At index {current_p} Point {p}: {has_projection}")
+
+            if has_projection and not dbv:
+                p_x1, p_x2 = proj[0][0], proj[1][0]
+                p_y1, p_y2 = proj[0][1], proj[1][1]
+                plt.plot([p_x1, p_x2], [p_y1, p_y2], 'yo--')
+                current_p = current_p + 1
+            elif dbv: # don't plot a double vertex, handle it in the reverse pass
+                current_p = current_p + 1
+            else:
+                break'''
+            
     plt.show()
 
     
@@ -103,8 +105,6 @@ def run():
         # a. start 
 
     # project all points from one line onto another
-
-    pass
 
 if __name__ == "__main__":
     run()
